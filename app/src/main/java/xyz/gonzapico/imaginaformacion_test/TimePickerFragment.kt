f58c2f27dc99d502adf67f50a098297fb2f0c825
Aperez <albertoperez@codisys.es>
2022-08-04 10:01:46 +0200

    Tema 12 Ejercicio 3

--- app/src/main/java/xyz/gonzapico/imaginaformacion_test/TimePickerFragment.kt
+++ app/src/main/java/xyz/gonzapico/imaginaformacion_test/TimePickerFragment.kt
@@ -18,12 +18,14 @@ class TimePickerFragment :
         val c = Calendar.getInstance()
         val hour = c.get(Calendar.HOUR_OF_DAY)
         val minute = c.get(Calendar.MINUTE)
-        val picker = TimePickerDialog(activity as Context,
-            object: TimePickerDialog.OnTimeSetListener {
-                override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
-                    Log.d("TimePickerFragment", "hora $hourOfDay minuto $minute")
-                }
-        }, hour, minute, true)
-        return picker
+        return TimePickerDialog(
+            activity as Context,
+            { _, hourOfDay, minute ->
+                Log.d(
+                    "TimePickerFragment",
+                    "hora $hourOfDay minuto $minute"
+                )
+            }, hour, minute, false
+        )
     }
 }
