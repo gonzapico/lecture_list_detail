5b3c2945f0e0b22e13188258913f9cc457e08e8f
dguerrero <dguerrero@codisys.es>
2022-08-04 12:50:24 +0200

    Formato AM/PM

--- app/src/main/java/xyz/gonzapico/imaginaformacion_test/TimePickerFragment.kt
+++ app/src/main/java/xyz/gonzapico/imaginaformacion_test/TimePickerFragment.kt
@@ -1,16 +1,17 @@
 package xyz.gonzapico.imaginaformacion_test
 
-import android.app.DatePickerDialog
+import android.R
 import android.app.Dialog
 import android.app.TimePickerDialog
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.widget.DatePicker
+import android.view.View
+import android.widget.Button
 import android.widget.TimePicker
 import androidx.fragment.app.DialogFragment
 import java.util.*
-import java.util.Calendar.YEAR
+
 
 class TimePickerFragment :
     DialogFragment(){
@@ -21,6 +22,24 @@ class TimePickerFragment :
         val picker = TimePickerDialog(activity as Context,
             object: TimePickerDialog.OnTimeSetListener {
                 override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+
+                    var am_pm = ""
+
+                    val datetime = Calendar.getInstance()
+                    datetime[Calendar.HOUR_OF_DAY] = hourOfDay
+                    datetime[Calendar.MINUTE] = minute
+
+                    if (datetime[Calendar.AM_PM] === Calendar.AM) am_pm =
+                        "AM" else if (datetime[Calendar.AM_PM] === Calendar.PM) am_pm = "PM"
+
+                    val strHrsToShow =
+                        if (datetime[Calendar.HOUR] === 0) "12" else datetime[Calendar.HOUR].toString() + ""
+
+                    (activity!!.findViewById<View>(R.id.btnEventStartTime) as Button).setText(
+                        strHrsToShow + ":" + datetime[Calendar.MINUTE] + " " + am_pm
+                    )
+
+
                     Log.d("TimePickerFragment", "hora $hourOfDay minuto $minute")
                 }
         }, hour, minute, true)
